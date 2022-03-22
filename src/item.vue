c3323ecaf01f61277a447d0674880af396f30a42
dai.dongpeng <swiftwithlove@163.com>
2022-03-22 17:16:31 +0800

    preview item.vue

---
+++ src/item.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="item" v-show="itemShow">
+        <Icon class="Icon1" :IconProps="IconProps"></Icon>
+        <Icon class="Icon2" :IconProps="IconProps1"></Icon>
+    </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator"
+import Icon from "./Icon.vue"
+
+@Component({
+    components: { Icon },
+})
+export default class item extends Vue {
+    IconProps = {
+        image: require("./assets/57256DB3-8DAA-4A71-B393-E3ACDA97F941.png"),
+        text: "全站免费",
+    }
+    IconProps1 = {
+        image: require("./assets/B070E62B-BE76-4657-BC71-56216339BEAB.png"),
+        text: "专属月票",
+    }
+    get itemShow() {
+        return true
+    }
+}
+</script>
+
+<style scoped>
+.item {
+    height: 103px;
+    align-items: flex-end;
+    align-self: stretch;
+    display: flex;
+    border-radius: 8px;
+    background-color: #ffffffff;
+}
+.Icon1 {
+    margin: 0px 0px 20px 22px;
+}
+.Icon2 {
+    margin: 0px 0px 20px 38px;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -7,18 +7,27 @@
 //   render: h => h(App),
 // }).$mount('#app')
 
-import Vue from "vue"
-import Main from "./Icon.vue"
+// import Vue from "vue"
+// import Main from "./Icon.vue"
 
-var propsData = {
-    IconProps: {
-        image: require("./assets/57256DB3-8DAA-4A71-B393-E3ACDA97F941.png"),
-        text: "全站免费",
-    },
-}
+// var propsData = {
+//     IconProps: {
+//         image: require("./assets/57256DB3-8DAA-4A71-B393-E3ACDA97F941.png"),
+//         text: "全站免费",
+//     },
+// }
+
+// new Vue({
+//     components: { Main },
+//     template: "<Main v-bind=propsData></Main>",
+//     data: { propsData: propsData },
+// }).$mount("#app")
+
+import Vue from "vue"
+import Main from "./item.vue"
 
 new Vue({
+    el: "#app",
     components: { Main },
-    template: "<Main v-bind=propsData></Main>",
-    data: { propsData: propsData },
-}).$mount("#app")
+    template: "<Main></Main>",
+})
