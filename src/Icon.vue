2709d2bfa551f209d69b639385ca4f05df2ccadd
dai.dongpeng <swiftwithlove@163.com>
2022-03-22 17:12:53 +0800

    preview Icon.vue

---
+++ src/Icon.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="Icon">
+        <div class="img1" :style="{ 'background-image': 'url(' + IconProps.image + ')' }"></div>
+        <span class="txt1" v-html="IconProps.text" v-on:click="txt1Action" v-on:focus="txt1Focus"></span>
+    </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator"
+
+@Component
+export default class Icon extends Vue {
+    @Prop() IconProps = {}
+    txt1Focus() {}
+    txt1Action() {}
+}
+</script>
+
+<style scoped>
+.Icon {
+    align-items: center;
+    align-self: flex-start;
+    display: flex;
+    flex-direction: column;
+}
+.img1 {
+    width: 31px;
+    height: 30px;
+    background: no-repeat center/100% 100%;
+}
+.txt1 {
+    margin-top: 7px;
+    font-size: 11px;
+    color: #929292ff;
+    white-space: nowrap;
+    letter-spacing: 0px;
+    font-family: "PingFang SC Regular";
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
-import Vue from 'vue'
-import App from './App.vue'
+// import Vue from 'vue'
+// import App from './App.vue'
 
-Vue.config.productionTip = false
+// Vue.config.productionTip = false
+
+// new Vue({
+//   render: h => h(App),
+// }).$mount('#app')
+
+import Vue from "vue"
+import Main from "./Icon.vue"
+
+var propsData = {
+    IconProps: {
+        image: require("./assets/57256DB3-8DAA-4A71-B393-E3ACDA97F941.png"),
+        text: "全站免费",
+    },
+}
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    components: { Main },
+    template: "<Main v-bind=propsData></Main>",
+    data: { propsData: propsData },
+}).$mount("#app")
